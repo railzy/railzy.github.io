61be1e1fde31d3958aaf278f0adc986489fa9f85
railzy <railzy@users.noreply.github.com>
2022-07-22 12:13:05 +0000

    deploy: railzy/ray233@acd42683594823ed13dea64f68952697d86503da

--- js/custom.js
+++ js/custom.js
@@ -2,57 +2,70 @@
  * Set the color theme
  * @param {string} theme
  */
- function setColorTheme (theme) {
+function setColorTheme(theme) {
   // set body attribute for CSS selector
-  document.body.setAttribute('theme', theme)
+  document.body.setAttribute("theme", theme);
   // set root color scheme
   // https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme
-  document.documentElement.style.setProperty('color-scheme', theme === 'light' ? 'light' : 'dark')
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    theme === "light" ? "light" : "dark"
+  );
   // save to local storage
-  window.localStorage && localStorage.setItem('theme', theme)
+  window.localStorage && localStorage.setItem("theme", theme);
   // set window.isDark for js
-  window.isDark = !(theme === 'light')
+  window.isDark = !(theme === "light");
 }
+
+// window.switchThemeEventSet = new Set();
 
 /**
  * Initialize the select theme button.
  */
- function initSelectTheme () {
-  Array.from(document.getElementsByClassName('color-theme-select')).forEach(themeSelect => {
-    // Get the current theme
-    const currentTheme = document.body.getAttribute('theme')
-    // Set the selected Index
-    for (let j = 0; j < themeSelect.options.length; j++) {
-      const i = themeSelect.options[j]
-      if (i.value === currentTheme) {
-        themeSelect.selectedIndex = j
-        break
-      }
-    }
-
-    themeSelect.addEventListener('change', () => {
-      const theme = themeSelect.value
-      window.localStorage && localStorage.setItem('theme', theme)
-      if (theme !== 'auto') {
-        setColorTheme(theme)
-      } else {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-          setColorTheme('dark')
-        } else {
-          setColorTheme('light')
+function initSelectTheme() {
+  Array.from(document.getElementsByClassName("color-theme-select")).forEach(
+    (themeSelect) => {
+      // Get the current theme
+      const currentTheme =
+        window.localStorage && localStorage.getItem("theme")
+          ? localStorage.getItem("theme")
+          : "auto";
+      // Set the selected Index
+      for (let j = 0; j < themeSelect.options.length; j++) {
+        const i = themeSelect.options[j];
+        if (i.value === currentTheme) {
+          themeSelect.selectedIndex = j;
+          break;
         }
       }
-      for (const event of window.switchThemeEventSet) event()
-    })
-  })
+
+      themeSelect.addEventListener("change", () => {
+        const theme = themeSelect.value;
+        window.localStorage && localStorage.setItem("theme", theme);
+        if (theme !== "auto") {
+          setColorTheme(theme);
+        } else {
+          window.localStorage && localStorage.setItem("theme", "auto");
+          if (
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+          ) {
+            document.body.setAttribute("theme", "dark");
+          } else {
+            document.body.setAttribute("theme", "light");
+          }
+        }
+      });
+    }
+  );
 }
 
-initSelectTheme ()
+initSelectTheme();
 
 // 监听暗色、亮色切换
 if (
-  (window.localStorage && !!localStorage.getItem("theme")) ||
-  (window.localStorage && localStorage.getItem("theme") === 'auto')
+  (window.localStorage && !localStorage.getItem("theme")) ||
+  (window.localStorage && localStorage.getItem("theme") === "auto")
 ) {
   let lightMedia = window.matchMedia("(prefers-color-scheme: light)");
 
@@ -62,10 +75,14 @@ if (
     let prefersDarkMode = e.matches;
 
     if (prefersDarkMode) {
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? document.body.setAttribute("theme", "dark")
-        : document.body.setAttribute("theme", "light");
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        document.body.setAttribute("theme", "dark");
+      } else {
+        document.body.setAttribute("theme", "light");
+      }
     }
   };
 
