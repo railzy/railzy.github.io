bcbf00952175a61a90d104e13f829bbacb4b76d5
railzy <railzy@users.noreply.github.com>
2022-07-22 11:34:16 +0000

    deploy: railzy/ray233@6a51844b3bed3da6ecf97203998100e8ef1914dc

---
+++ js/custom.js
@@ -0,0 +1,80 @@
+/**
+ * Set the color theme
+ * @param {string} theme
+ */
+ function setColorTheme (theme) {
+  // set body attribute for CSS selector
+  document.body.setAttribute('theme', theme)
+  // set root color scheme
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/color-scheme
+  document.documentElement.style.setProperty('color-scheme', theme === 'light' ? 'light' : 'dark')
+  // save to local storage
+  window.localStorage && localStorage.setItem('theme', theme)
+  // set window.isDark for js
+  window.isDark = !(theme === 'light')
+}
+
+/**
+ * Initialize the select theme button.
+ */
+ function initSelectTheme () {
+  Array.from(document.getElementsByClassName('color-theme-select')).forEach(themeSelect => {
+    // Get the current theme
+    const currentTheme = document.body.getAttribute('theme')
+    // Set the selected Index
+    for (let j = 0; j < themeSelect.options.length; j++) {
+      const i = themeSelect.options[j]
+      if (i.value === currentTheme) {
+        themeSelect.selectedIndex = j
+        break
+      }
+    }
+
+    themeSelect.addEventListener('change', () => {
+      const theme = themeSelect.value
+      window.localStorage && localStorage.setItem('theme', theme)
+      if (theme !== 'auto') {
+        setColorTheme(theme)
+      } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          setColorTheme('dark')
+        } else {
+          setColorTheme('light')
+        }
+      }
+      for (const event of window.switchThemeEventSet) event()
+    })
+  })
+}
+
+initSelectTheme ()
+
+// 监听暗色、亮色切换
+if (
+  (window.localStorage && !!localStorage.getItem("theme")) ||
+  (window.localStorage && localStorage.getItem("theme") === 'auto')
+) {
+  let lightMedia = window.matchMedia("(prefers-color-scheme: light)");
+
+  let darkMedia = window.matchMedia("(prefers-color-scheme: dark)");
+
+  let callback = (e) => {
+    let prefersDarkMode = e.matches;
+
+    if (prefersDarkMode) {
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? document.body.setAttribute("theme", "dark")
+        : document.body.setAttribute("theme", "light");
+    }
+  };
+
+  if (
+    typeof darkMedia.addEventListener === "function" ||
+    typeof lightMedia.addEventListener === "function"
+  ) {
+    lightMedia.addEventListener("change", callback);
+    darkMedia.addEventListener("change", callback);
+  }
+}
+// 监听暗色、亮色切换End
