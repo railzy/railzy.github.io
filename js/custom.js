a28397827f8bf628d25941b0f4c1e81b1de685d7
railzy <railzy@users.noreply.github.com>
2024-06-01 18:18:47 +0000

    deploy: railzy/ray233@cc0b8a9a3d98fa229b5e7aa20ff5154aa455462b

--- js/custom.js
+++ js/custom.js
@@ -22,45 +22,45 @@
 // /**
 //  * Initialize the select theme button.
 //  */
-// function initSelectTheme() {
-//   Array.from(document.getElementsByClassName("color-theme-select")).forEach(
-//     (themeSelect) => {
-//       // Get the current theme
-//       const currentTheme =
-//         window.localStorage && localStorage.getItem("theme")
-//           ? localStorage.getItem("theme")
-//           : "auto";
-//       // Set the selected Index
-//       for (let j = 0; j < themeSelect.options.length; j++) {
-//         const i = themeSelect.options[j];
-//         if (i.value === currentTheme) {
-//           themeSelect.selectedIndex = j;
-//           break;
-//         }
-//       }
+function initSelectTheme() {
+  Array.from(document.getElementsByClassName("color-theme-select")).forEach(
+    (themeSelect) => {
+      // Get the current theme
+      const currentTheme =
+        window.localStorage && localStorage.getItem("theme")
+          ? localStorage.getItem("theme")
+          : "auto";
+      // Set the selected Index
+      for (let j = 0; j < themeSelect.options.length; j++) {
+        const i = themeSelect.options[j];
+        if (i.value === currentTheme) {
+          themeSelect.selectedIndex = j;
+          break;
+        }
+      }
 
-//       themeSelect.addEventListener("change", () => {
-//         const theme = themeSelect.value;
-//         window.localStorage && localStorage.setItem("theme", theme);
-//         if (theme !== "auto") {
-//           setColorTheme(theme);
-//         } else {
-//           window.localStorage && localStorage.removeItem("theme");
-//           if (
-//             window.matchMedia &&
-//             window.matchMedia("(prefers-color-scheme: dark)").matches
-//           ) {
-//             document.body.setAttribute("theme", "dark");
-//           } else {
-//             document.body.setAttribute("theme", "light");
-//           }
-//         }
-//       });
-//     }
-//   );
-// }
+      themeSelect.addEventListener("change", () => {
+        const theme = themeSelect.value;
+        window.localStorage && localStorage.setItem("theme", theme);
+        if (theme !== "auto") {
+          setColorTheme(theme);
+        } else {
+          window.localStorage && localStorage.removeItem("theme");
+          if (
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+          ) {
+            document.body.setAttribute("theme", "dark");
+          } else {
+            document.body.setAttribute("theme", "light");
+          }
+        }
+      });
+    }
+  );
+}
 
-// initSelectTheme();
+initSelectTheme();
 
 // 监听暗色、亮色切换
 if (
